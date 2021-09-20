aa97040a2c8ea53bc1d24d12c753daf69b9c5b87
Farhad <FarhadDeveloper98@gmail.com>
2021-09-20 09:40:35 +0430

    commit3 Add EndPoints.kt

---
+++ app/src/main/java/com/example/clean_architecture_mvvm_news_app/api/EndPoints.kt
@@ -0,0 +1,9 @@
+package com.example.clean_architecture_mvvm_news_app.api
+
+object EndPoints {
+
+    const val HEAD_LINES = "v2/top-headlines"
+    const val SEARCH_IN_EVERYTHING = "v2/everything"
+
+
+}
